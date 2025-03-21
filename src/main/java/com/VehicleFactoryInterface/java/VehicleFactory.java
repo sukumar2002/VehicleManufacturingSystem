b67d55bf5058676de98f7f8a076c3264f450e838
sukumar2002 <127292893+sukumar2002@users.noreply.github.com>
2025-03-21 18:34:26 +0530

    Update VehicleFactory.java

--- src/main/java/com/VehicleFactoryInterface/java/VehicleFactory.java
+++ src/main/java/com/VehicleFactoryInterface/java/VehicleFactory.java
@@ -1,9 +1,5 @@
 package com.VehicleFactoryInterface.java;
-
 import com.VehicleInterface.java.Vehicle;
-
 public interface VehicleFactory {
-      
-
 	Vehicle createVehicle(String type, String engineType);        
 }
